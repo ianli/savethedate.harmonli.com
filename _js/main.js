9470119327ed9d2657f4f3fc015da7eb7b105f94
Ian Li <ianarli@gmail.com>
2015-08-29 01:58:47 -0700

    Renamed reveal() to revealSaveTheDateCard().

--- _js/main.js
+++ _js/main.js
@@ -11,7 +11,7 @@ $(document).ready(function() {
   initialize();
 
   _.delay(function() {
-    reveal();
+    revealSaveTheDateCard();
   }, 500);
 
   $(window).scroll(_.throttle(function() {
@@ -37,7 +37,7 @@ function initialize() {
   });
 }
 
-function reveal() {
+function revealSaveTheDateCard() {
   $('.app-bg-obfuscator')
       .velocity({
         opacity: OBFUSCATOR_OPACITY
