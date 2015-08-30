150ad00a88c29a2a8e7836ceffeb452d85f157f5
Ian Li <ianarli@gmail.com>
2015-08-29 18:39:23 -0700

    Added triggerHook to ScrollScene options.

--- _js/ScrollScene.js
+++ _js/ScrollScene.js
@@ -7,14 +7,20 @@ var ScrollScene = function(options) {
   // Selector whose location will trigger the callback.
   this.triggerElement = options.triggerElement;
 
+  // Trigger can happen when the element:
+  // - is in the center of the viewport (onCenter) [default]
+  // - enters the viewport (onEnter)
+  // - leaves the viewport (onLeave)
+  this.triggerHook = options.triggerHook || 'onCenter';
+
+  // Offset (in pixels) from the trigger point.
+  this.triggerOffset = options.triggerOffset || 0;
+
   // Called when we reach the triggerElement while scrolling DOWN.
   this.downCallback = options.downCallback || function() {};
 
   // Called when we reach the triggerElement while scrolling UP.
   this.upCallback = options.upCallback || function() {};
-
-  // Offset from the trigger point.
-  this.triggerOffset = options.triggerOffset || 0;
 
   // Allow triggering down.
   this.allowDownCallback = true;
@@ -33,9 +39,25 @@ var ScrollScene = function(options) {
  *                 - scrollTop
  */
 ScrollScene.prototype.trigger = function(scrollInfo) {
-  var triggerPoint = $(this.triggerElement).offset().top -
-      (scrollInfo.windowHeight / 2) +
-      this.triggerOffset;
+  // The trigger point depends on the location of the element,
+  // the elements location in the viewport (onEnter, onCenter, onLeave),
+  // and the trigger offset.
+  var triggerPoint;
+  switch (this.triggerHook) {
+    case 'onEnter':
+      triggerPoint = $(this.triggerElement).offset().top -
+          scrollInfo.windowHeight +
+          this.triggerOffset;
+    case 'onLeave':
+      triggerPoint = $(this.triggerElement).offset().top -
+          this.triggerOffset;
+      break;
+    case 'onCenter':
+    default:
+      triggerPoint = $(this.triggerElement).offset().top -
+          (scrollInfo.windowHeight / 2) +
+          this.triggerOffset;
+  }
 
   if (this.allowDownCallback &&
       scrollInfo.direction == 'down' &&
