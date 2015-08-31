f136def3b92c57d28d6a8e9e5deb0f229f4bf524
Ian Li <ianarli@gmail.com>
2015-08-31 15:09:10 -0700

    Use velocity to animate hanging icons instead of CSS.

--- _js/HangingIcons.js
+++ _js/HangingIcons.js
@@ -11,30 +11,56 @@ var HangingIcons = {
 };
 
 function init() {
+  $('.app-string')
+    .css({
 
+    });
 }
 
 function show() {
-  $('#app-string--sf').addClass('reveal');
+  var props = {
+    top: 0
+  };
+
+  var options = {
+    duration: 500,
+    easing: [120, 15]
+  };
+
+  $('#app-string--sf')
+      .velocity(props, options);
 
   _.delay(function() {
-    $('#app-string--heart').addClass('reveal');
+    $('#app-string--heart')
+        .velocity(props, options);
   }, 100);
 
   _.delay(function() {
-    $('#app-string--happy').addClass('reveal');
+    $('#app-string--happy')
+        .velocity(props, options);
   }, 200);
 }
 
 function hide() {
-  $('#app-string--happy').removeClass('reveal');
+  var props = {
+    top: -$(window).height() / 2
+  };
+
+  var options = {
+    duration: 200
+  };
+
+  $('#app-string--happy')
+      .velocity(props, options);
 
   _.delay(function() {
-    $('#app-string--heart').removeClass('reveal');
+    $('#app-string--heart')
+        .velocity(props, options);
   }, 100);
 
   _.delay(function() {
-    $('#app-string--sf').removeClass('reveal');
+    $('#app-string--sf')
+        .velocity(props, options);
   }, 200);
 }
 
